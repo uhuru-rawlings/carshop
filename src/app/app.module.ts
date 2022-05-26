8a6ce3f3922c3e522b503da0babf26b14a20ad63
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-26 22:26:51 +0300

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule,routingLinks } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgImageSliderModule } from 'ng-image-slider';
 
 import { ToastrModule } from 'ngx-toastr';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingLinks
+    routingLinks,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(),
-    NgImageSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
