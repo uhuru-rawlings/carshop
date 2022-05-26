99bc53293e5d0d71fd1835c3d9250c0c08ba2be4
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-26 21:48:12 +0300

    added image slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:HomepageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'resetpassword',component:ResetpasswordComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingLinks = [LoginComponent,SignupComponent,ResetpasswordComponent,HomepageComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingLinks } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 import { ToastrModule } from 'ngx-toastr';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
-    ResetpasswordComponent,
-    HomepageComponent
+    routingLinks
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(),
+    NgImageSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
