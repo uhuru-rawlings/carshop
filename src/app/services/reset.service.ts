41d9805d7faad9239adf8c4d3e08d3fc3539e1f2
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-28 11:15:29 +0300

    reset password function

--- src/app/services/reset.service.ts
+++ src/app/services/reset.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResetService {
+  url:any = 'https://carapis.herokuapp.com/account/resetpassword'
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  restPassword(details:any):Observable<any>{
+    return this.http.post(this.url, details)
+  }
 }
